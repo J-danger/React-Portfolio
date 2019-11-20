69d5a0087b7eeb8d2e7880ccb5bc0299c81ec290
Stephen Howe <Stevehowe10@gmail.com>
2019-11-19 19:10:15 -0700

    cards centered

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -12,28 +12,12 @@ class Contact extends Component {
     <>
       <div className="layer">
         <NavBar/>
-        <div className="animated fadeIn slow">
           <div id="contact-form">
-
-        <ContactForm />
-        </div>
-          {/* <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-            <div className="form-group">
-                <label for="name">Name</label>
-                <input type="text" className="form-control" id="name" />
-            </div>
-            <div className="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
-            </div>
-            <div className="form-group">
-                <label for="message">Message</label>
-                <textarea className="form-control" rows="5" id="message"></textarea>
-            </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-          </form> */}
-                </div>
-        </div>
+            <div className="animated fadeIn slow">          
+              <ContactForm />
+            </div>          
+          </div>         
+      </div>
         <Particles
   params={{
     particles: {
